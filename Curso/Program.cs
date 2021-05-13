71544d30d3717957e7f5f29bf4217794e0c6deee
farleybsd <farley.t.i@hotmail.com>
2021-05-12 23:53:42 -0300

    Criando um projeto via CLI

---
+++ Curso/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DominandoEfCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
