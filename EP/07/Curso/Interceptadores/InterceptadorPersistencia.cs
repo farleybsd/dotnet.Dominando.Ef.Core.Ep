0e0bf34d3c5e127b7f6ae8b1584db8bf56b15511
farleybsd <farley.t.i@hotmail.com>
2021-05-13 00:40:33 -0300

    Aula07

---
+++ EP/07/Curso/Interceptadores/InterceptadorPersistencia.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace Curso.Interceptadores
+{
+    public class InterceptadorPersistencia : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(
+            DbContextEventData eventData, 
+            InterceptionResult<int> result)
+        {
+            
+            System.Console.WriteLine(eventData.Context.ChangeTracker.DebugView.LongView);
+
+            return result;
+        }
+    }
+}
---
+++ EP/07/Curso/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Curso.Domain;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Storage;
+
+namespace DominandoEFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TesteInterceptacao();
+            TesteInterceptacaoSaveChanges();
+        }       
+
+        static void TesteInterceptacaoSaveChanges()
+        {
+            using (var db = new Curso.Data.ApplicationContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                db.Funcoes.Add(new Funcao
+                {
+                    Descricao1 = "Teste"
+                });
+
+                db.SaveChanges();
+            }
+        }
+
+        static void TesteInterceptacao()
+        {
+            using (var db = new Curso.Data.ApplicationContext())
+            {
+                var consulta = db
+                    .Funcoes
+                    .TagWith("Use NOLOCK")
+                    .FirstOrDefault(); 
+
+                Console.WriteLine($"Consulta: {consulta?.Descricao1}");
+            }
+        }
+    }
+}
