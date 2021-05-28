886c2b05c7d3934500a671a188702f63577c72f2
farleybsd <farley.t.i@hotmail.com>
2021-05-28 00:34:53 -0300

    Gravando seus logs em um arquivo

--- HabilbitandoLog.EF.CORE/Data/ApplicationContext.cs
+++ HabilbitandoLog.EF.CORE/Data/ApplicationContext.cs
@@ -3,11 +3,13 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
 
 namespace HabilbitandoLog.EF.CORE.Data
 {
     public class ApplicationContext : DbContext
     {
+        private readonly StreamWriter writer = new StreamWriter("meu_log_ef_core.txt",append:true);
         public DbSet<Departamento> Departamentos { get; set; }
         public DbSet<Funcionario> Funcionarios { get; set; }
 
@@ -17,15 +19,22 @@ namespace HabilbitandoLog.EF.CORE.Data
 
             optionsBuilder
                 .UseSqlServer(strConnection)
+                .LogTo(writer.WriteLine,LogLevel.Information);
+                /*
                 .LogTo(Console.WriteLine,
                 new[] {CoreEventId.ContextInitialized,RelationalEventId.CommandExecuted },
                 LogLevel.Information,
                 DbContextLoggerOptions.LocalTime | DbContextLoggerOptions.SingleLine
-                );
+                );*/ // pega evento por tipo de Log
                 //.LogTo(Console.WriteLine,LogLevel.Information); // coletando os log do ef no console
                 
                 
                
+        }
+        public override void Dispose()
+        {
+            base.Dispose();
+            writer.Dispose();
         }
 
     }
