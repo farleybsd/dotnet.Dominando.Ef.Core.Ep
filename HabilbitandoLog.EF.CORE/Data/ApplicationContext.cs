e524213722d638f0b1880158a445d2bcb321f6af
farleybsd <farley.t.i@hotmail.com>
2021-05-28 00:03:43 -0300

    Configurando um log simplificado

---
+++ HabilbitandoLog.EF.CORE/Data/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using HabilbitandoLog.EF.CORE.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace HabilbitandoLog.EF.CORE.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=DevIo-02;Integrated Security=True;pooling=true";
+
+            optionsBuilder
+                .UseSqlServer(strConnection)
+                .LogTo(Console.WriteLine,LogLevel.Information); // coletando os log do ef no console
+                
+                
+               
+        }
+
+    }
+}
---
+++ HabilbitandoLog.EF.CORE/Program.cs
@@ -0,0 +1,22 @@
+﻿using HabilbitandoLog.EF.CORE.Data;
+using System;
+using System.Linq;
+
+namespace HabilbitandoLog.EF.CORE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConsultarDepartamentos();
+            Console.ReadKey();
+        }
+
+        static void ConsultarDepartamentos()
+        {
+            using var db = new  ApplicationContext();
+
+            var departamento = db.Departamentos.Where(p => p.Id > 0).ToArray();
+        }
+    }
+}
