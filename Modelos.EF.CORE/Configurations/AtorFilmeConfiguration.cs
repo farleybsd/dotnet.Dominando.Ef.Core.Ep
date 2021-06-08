53f62ee9514a2224ed91dfec7f51b1604d01571c
farleybsd <farley.t.i@hotmail.com>
2021-06-08 01:09:01 -0300

    Customização muitos-para-muitos

--- Modelos.EF.CORE/Configurations/AtorFilmeConfiguration.cs
+++ Modelos.EF.CORE/Configurations/AtorFilmeConfiguration.cs
@@ -14,11 +14,24 @@ namespace Modelos.EF.CORE.Configurations
     {
         public void Configure(EntityTypeBuilder<Ator> builder)
         {
+            //builder
+            //    .HasMany(p => p.Filmes)
+            //    .WithMany(p => p.Atores)
+            //    .UsingEntity(p=>p.ToTable("AtoresFilmes"));
+
             builder
                 .HasMany(p => p.Filmes)
                 .WithMany(p => p.Atores)
-                .UsingEntity(p=>p.ToTable("AtoresFilmes"));
-                
+                .UsingEntity<Dictionary<string,object>>(
+                "FilmesAtores", // nome da tabela
+                p=>p.HasOne<Filme>().WithMany().HasForeignKey("FilmeId"),
+                p => p.HasOne<Ator>().WithMany().HasForeignKey("AtorId"),
+                p =>
+                {
+                    p.Property<DateTime>("CadastradoEm").HasDefaultValueSql("GETDATE()");
+                }
+                );
+
         }
     }
 }
