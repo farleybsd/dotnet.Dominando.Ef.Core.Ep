d5b5525c51d0d11484f46a7b5dbe83af2d583f69
farleybsd <farley.t.i@hotmail.com>
2021-06-10 00:27:05 -0300

    Configurando modelo de dados do TPT

--- Modelos.EF.CORE/Configurations/PessoaConfiguration.cs
+++ Modelos.EF.CORE/Configurations/PessoaConfiguration.cs
@@ -11,14 +11,47 @@ namespace Modelos.EF.CORE.Configurations
 {
     public class PessoaConfiguration : IEntityTypeConfiguration<Pessoa>
     {
+        //TPH
+        //public class PessoaConfiguration : IEntityTypeConfiguration<Pessoa>
+        //{
+        //    public void Configure(EntityTypeBuilder<Pessoa> builder)
+        //    {
+        //        builder
+        //            .ToTable("Pessoa")
+        //            .HasDiscriminator<int>("TipoPessoa")
+        //            .HasValue<Pessoa>(3)
+        //            .HasValue<Instrutor>(6)
+        //            .HasValue<Aluno>(99);
+        //    }
+        //}
+
+        // TPT TABELAS POR TIPO  BOA PATRICA
         public void Configure(EntityTypeBuilder<Pessoa> builder)
         {
             builder
-                .ToTable("Pessoa")
-                .HasDiscriminator<int>("TipoPessoa")
-                .HasValue<Pessoa>(3)
-                .HasValue<Instrutor>(6)
-                .HasValue<Aluno>(99);                
+                .ToTable("Pessoa");
+                                
+        }
+    }
+
+    public class InstrutorConfiguration : IEntityTypeConfiguration<Instrutor>
+    {
+        public void Configure(EntityTypeBuilder<Instrutor> builder)
+        {
+            builder
+                .ToTable("Instrutores");
+                
+        }
+    }
+
+
+    public class AlunoConfiguration : IEntityTypeConfiguration<Aluno>
+    {
+        public void Configure(EntityTypeBuilder<Aluno> builder)
+        {
+            builder
+                .ToTable("Alunos");
+               
         }
     }
 }
--- Modelos.EF.CORE/Domain/Pessoa.cs
+++ Modelos.EF.CORE/Domain/Pessoa.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Modelos.EF.CORE.Domain
 {
-    public class Pessoa
+    public class Pessoa // TPH Tabela  por Hierarquia cria uma unica tabela
     {
         public int Id { get; set; }
         public string Nome { get; set; }
