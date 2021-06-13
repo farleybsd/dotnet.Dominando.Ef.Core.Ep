cc3aadc0600645447ea78a4307ef083357f973f2
farleybsd <farley.t.i@hotmail.com>
2021-06-13 18:14:16 -0300

    Funções de datas

---
+++ Functions/Data/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using Functions.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Funcao> Funcoes { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data source=(localdb)\\mssqllocaldb; Initial Catalog=DevIO-02;Integrated Security=true;pooling=true;";
+
+            optionsBuilder
+                .UseSqlServer(strConnection)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,78 @@
+﻿using Functions.Data;
+using Functions.Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace Functions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            FuncoesDeDatas();
+            Console.ReadKey();
+        }
+        
+        static void ApagarCriarBancoDeDados()
+        {
+            using var db = new ApplicationContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            db.Funcoes.AddRange(
+            new Funcao
+            {
+                Data1 = DateTime.Now.AddDays(2),
+                Data2 = "2021-01-01",
+                Descricao1 = "Bala 1 ",
+                Descricao2 = "Bala 1 "
+            },
+            new Funcao
+            {
+                Data1 = DateTime.Now.AddDays(1),
+                Data2 = "XX21-01-01",
+                Descricao1 = "Bola 2",
+                Descricao2 = "Bola 2"
+            },
+            new Funcao
+            {
+                Data1 = DateTime.Now.AddDays(1),
+                Data2 = "XX21-01-01",
+                Descricao1 = "Tela",
+                Descricao2 = "Tela"
+            });
+
+            db.SaveChanges();
+        }
+        static void FuncoesDeDatas()
+        {
+            ApagarCriarBancoDeDados();
+
+            using (var db = new ApplicationContext())
+            {
+                var script = db.Database.GenerateCreateScript();
+
+                Console.WriteLine(script);
+
+                var dados = db.Funcoes.AsNoTracking().Select(p =>
+                   new
+                   {
+
+                       Dias = EF.Functions.DateDiffDay(DateTime.Now, p.Data1),
+                       Meses = EF.Functions.DateDiffMonth(DateTime.Now, p.Data1),
+                       Data = EF.Functions.DateFromParts(2021, 1, 2),
+                       DataValida = EF.Functions.IsDate(p.Data2),
+                   });
+
+                foreach (var f in dados)
+                {
+                    Console.WriteLine(f);
+                }
+
+            }
+        }
+    }
+
+}
