0886638b6f0c5a0824341a97a59ac5b9f809c6be
farleybsd <farley.t.i@hotmail.com>
2021-06-18 00:04:36 -0300

    Salvando ponto de uma transação

--- Transacoes/Program.cs
+++ Transacoes/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using Transacoes.Data;
 using Transacoes.Domain;
@@ -12,10 +13,55 @@ namespace Transacoes
             //ComportamentoPadrao();
             //GerenciandoTransacaoManualmente();
             //ReverterTransacao();
-            ReverterTransacao();
+            //ReverterTransacao();
+            SalvarPontoTransacao();
             Console.ReadKey();
         }
+        static void SalvarPontoTransacao()
+        {
+            CadastrarLivro();
 
+            using (var db = new ApplicationContext())
+            {
+                using var transacao = db.Database.BeginTransaction();
+                try
+                {
+                    var livro = db.Livros.FirstOrDefault(p=>p.Id==1);
+                    livro.Autor = "Rafael Almeida";
+                    db.SaveChanges();
+
+                    transacao.CreateSavepoint("desfazer_apenas_insercao"); // Ponto que ira voltar se der erro
+
+                    db.Livros.Add(
+                        new Livro 
+                        { 
+                            Titulo = "ASP.NET Core Enterprise Application",
+                            Autor="Eduardo Pires"
+                        });
+
+                    db.SaveChanges();
+
+                    db.Livros.Add(
+                        new Livro
+                        {
+                            Titulo = "Dominando Entity Framework Core",
+                            Autor = "Rafael Almeida".PadLeft(16,'*') // erro simulado
+                        });
+
+                    db.SaveChanges();
+                    transacao.Commit();
+                }
+                catch (DbUpdateException e)
+                {
+                    transacao.RollbackToSavepoint("desfazer_apenas_insercao"); // Aplica o ponto de retorno
+
+                    if (e.Entries.Count(p=>p.State == EntityState.Added) == e.Entries.Count)
+                    {
+                        transacao.Commit();
+                    }
+                }
+            }
+        }
         static void GerenciandoTransacaoManualmente()
         {
             CadastrarLivro();
