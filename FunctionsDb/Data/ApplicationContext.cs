00b344bd44f4801ff1591ea8c43b902d7a53f0b5
farleybsd <farley.t.i@hotmail.com>
2021-06-21 11:10:06 -0300

    Built-In Function

---
+++ FunctionsDb/Data/ApplicationContext.cs
@@ -0,0 +1,29 @@
+﻿using FunctionsDb.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionsDb.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Livro> Livros { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data source=(localdb)\\mssqllocaldb; Initial Catalog=DevIO-02;Integrated Security=true;pooling=true;";
+
+            optionsBuilder
+                .UseSqlServer(strConnection)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+
+        [DbFunction(name:"LEFT",schema:"",IsBuiltIn =true)] // funcao incorporada do db funcao nativa
+        public static string Left(string dados,int quantidade)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FunctionsDb/Program.cs
@@ -0,0 +1,49 @@
+﻿using FunctionsDb.Data;
+using System;
+using System.Linq;
+
+namespace FunctionsDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuncaoLeft();
+
+            Console.ReadKey();
+        }
+
+        static void FuncaoLeft()
+        {
+            //Built-In Function
+            // funcao incorporada do db funcao nativa
+            CadastrarLivro();
+
+            using var db = new ApplicationContext();
+
+            var resultado = db.Livros.Select(p=> ApplicationContext.Left(p.Titulo,10));
+
+            foreach (var parteTitulo in resultado)
+            {
+                Console.WriteLine(parteTitulo);
+            }
+        }
+        static void CadastrarLivro()
+        {
+            using (var db = new ApplicationContext())
+            {
+                db.Database.EnsureDeleted();
+                db.Database.EnsureCreated();
+
+                db.Livros.Add(
+                    new Domain.Livro
+                    {
+                        Titulo = "Introdução ao Entity Framework Core",
+                        Autor = "Rafael"
+                    });
+
+                db.SaveChanges();
+            }
+        }
+    }
+}
