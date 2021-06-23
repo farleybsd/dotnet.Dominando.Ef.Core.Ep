0159420ee03a71048487dabc2a1fbf7a017e9ec7
farleybsd <farley.t.i@hotmail.com>
2021-06-23 10:18:20 -0300

    Resolução de Identidade

---
+++ Performace/Data/ApplicationContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Performace.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Performace.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string strConnection = "Data source=(localdb)\\mssqllocaldb; Initial Catalog=DevIO-02;Integrated Security=true;pooling=true;";
+
+            optionsBuilder
+                .UseSqlServer(strConnection)
+                .LogTo(Console.WriteLine, LogLevel.Information)
+                .EnableSensitiveDataLogging();
+        }
+    }
+}
---
+++ Performace/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Performace.Data;
+using Performace.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Performace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Setup();
+            //ConsultaRastreada();
+            //ConsultaNaoRastreada();
+            ConsultaComResolucaoIdentidade();
+            Console.ReadKey();
+        }
+        static void Setup()
+        {
+            using var db = new ApplicationContext();
+            db.Database.EnsureDeleted();
+            db.Database.EnsureCreated();
+
+            db.Departamentos.Add(new Departamento { 
+            
+                Descricao = "Departamento Teste",
+                Ativo = true,
+                Funcionarios = Enumerable.Range(1,100).Select(p => new Funcionario // vai criar 100 funcionario automaticamente
+                {
+                    Cpf = p.ToString().PadLeft(11,'0'),
+                    Nome = $"Funcionario {p}",
+                    Rg = p.ToString()
+                }).ToList()
+            
+            });
+
+            db.SaveChanges();
+        }
+        static void ConsultaRastreada()
+        {
+            //vai no banco faz uma copia dos resultados cria um snapshot
+            // e usa o contexto como cache sem fazer new para cada include
+            using var db = new ApplicationContext();
+
+            var funcionario = db.Funcionarios.Include(p => p.Departamento).ToList();
+        }
+
+        static void ConsultaNaoRastreada()
+        {
+            // Consulta somente leitura
+            // Ele faz um select no banco e da um  new para cada include
+            using var db = new ApplicationContext();
+
+            var funcionario = db.Funcionarios.AsNoTracking().Include(p => p.Departamento).ToList();
+        }
+
+        static void ConsultaComResolucaoIdentidade()
+        {
+            // Consulta somente leitura
+            // Ele faz um select no banco sem da um  new para cada include
+            // sem rastrear
+            using var db = new ApplicationContext();
+
+            var funcionario = db.Funcionarios
+                .AsNoTrackingWithIdentityResolution()
+                .Include(p => p.Departamento)
+                .ToList();
+        }
+    }
+}
