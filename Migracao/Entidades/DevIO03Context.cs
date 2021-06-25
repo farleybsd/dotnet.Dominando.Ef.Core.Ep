8ddcf5c096e409a4be1a828eb061991a990a488b
farleybsd <farley.t.i@hotmail.com>
2021-06-25 16:18:02 -0300

    Engenharia reversa

---
+++ Migracao/Entidades/DevIO03Context.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Meu.namespace
+{
+    public partial class DevIO03Context : DbContext
+    {
+        public DevIO03Context()
+        {
+        }
+
+        public DevIO03Context(DbContextOptions<DevIO03Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Pessoa> Pessoas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data source=(localdb)\\mssqllocaldb; Initial Catalog=DevIO-03;Integrated Security=true;pooling=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            modelBuilder.Entity<Pessoa>(entity =>
+            {
+                entity.Property(e => e.Nome).IsUnicode(false);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Migracao/Entidades/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace Meu.namespace
+{
+    public partial class Pessoa
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(60)]
+        public string Nome { get; set; }
+    }
+}
