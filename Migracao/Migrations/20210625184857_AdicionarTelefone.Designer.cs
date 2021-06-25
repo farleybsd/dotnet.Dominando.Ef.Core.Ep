4ae7674da478277251ef47f5ac54c86157628d24
farleybsd <farley.t.i@hotmail.com>
2021-06-25 15:57:09 -0300

    Migrações pendentes

--- Migracao/Migrations/20210625184857_AdicionarTelefone.Designer.cs
+++ Migracao/Migrations/20210625184857_AdicionarTelefone.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Migracao.Data;
 
 namespace Migracao.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20210625184857_AdicionarTelefone")]
+    partial class AdicionarTelefone
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Migracao/Program.cs
+++ Migracao/Program.cs
@@ -9,7 +9,16 @@ namespace Migracao
         static void Main(string[] args)
         {
             using var db = new ApplicationContext();
-            db.Database.Migrate();
+            //db.Database.Migrate();
+
+            var migracoes = db.Database.GetPendingMigrations();
+
+            foreach (var item in migracoes)
+            {
+                Console.WriteLine($"Migracao Pendente: {item}");
+            }
+
+            Console.ReadKey();
         }
     }
 }
