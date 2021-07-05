74fcfec0745a49ce820fde952c624333e1dab045
farleybsd <farley.t.i@hotmail.com>
2021-07-05 14:38:57 -0300

    Include com consultas filtradas

--- DicasETruques/DicasETruques/EF.CORE.DICASETRUQUES/Program.cs
+++ DicasETruques/DicasETruques/EF.CORE.DICASETRUQUES/Program.cs
@@ -13,13 +13,26 @@ namespace EF.CORE.DICASETRUQUES
         {
             //ToQueryString();
             //DebugView();
-            ClearContext();
+            //ClearContext();
+            ConsultaFiltrada();
+        }
+        static void ConsultaFiltrada()
+        {
+            using var db = new ApplicationContext();
+
+            var sql = db.Departamentos.Include(p =>
+                p.colaboradores
+                .Where(c => c.Descricao.Contains("Teste"))
+                   ).ToQueryString();
+
+            Console.WriteLine(sql);
+            Console.ReadKey();
         }
         static void ClearContext()
         {
             using var db = new ApplicationContext();
 
-            
+
 
 
             db.Departamentos.Add(new Departamento
@@ -39,24 +52,25 @@ namespace EF.CORE.DICASETRUQUES
 
             //db.Database.EnsureCreated();
 
-            
-            db.Departamentos.Add( new Departamento {
-            Descricao = "Teste - DebugView "
-           
+
+            db.Departamentos.Add(new Departamento
+            {
+                Descricao = "Teste - DebugView "
+
             });
 
             var query = db.Departamentos.Where(p => p.Id > 2);
 
-            
+
             Console.ReadKey();
         }
         static void ToQueryString()
         {
             using var db = new ApplicationContext();
-            
+
             db.Database.EnsureCreated();
 
-            var query = db.Departamentos.Where(p=> p.Id > 2);
+            var query = db.Departamentos.Where(p => p.Id > 2);
 
             var sql = query.ToQueryString();
 
