47e2b62f4278bc7c85b282589364f0bc6402cde8
farleybsd <farley.t.i@hotmail.com>
2021-07-05 12:17:23 -0300

    Método ToQueryString

--- DicasETruques/DicasETruques/EF.CORE.DICASETRUQUES/Program.cs
+++ DicasETruques/DicasETruques/EF.CORE.DICASETRUQUES/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EF.CORE.DICASETRUQUES.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
 
 namespace EF.CORE.DICASETRUQUES
 {
@@ -6,7 +9,21 @@ namespace EF.CORE.DICASETRUQUES
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ToQueryString();
+        }
+
+        static void ToQueryString()
+        {
+            using var db = new ApplicationContext();
+            
+            db.Database.EnsureCreated();
+
+            var query = db.Departamentos.Where(p=> p.Id > 2);
+
+            var sql = query.ToQueryString();
+
+            Console.WriteLine(sql);
+            Console.ReadKey();
         }
     }
 }
