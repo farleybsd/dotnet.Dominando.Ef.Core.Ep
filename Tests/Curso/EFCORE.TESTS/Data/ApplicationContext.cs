da71842cfabf16acf4fdcb2d6f9fe17e0c682191
farleybsd <farley.t.i@hotmail.com>
2021-07-06 00:05:46 -0300

    Criando entidade e contexto

---
+++ Tests/Curso/EFCORE.TESTS/Data/ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using EFCORE.TESTS.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCORE.TESTS.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options) :base(options)
+        {
+
+        }
+    }
+}
---
+++ Tests/Curso/EFCORE.TESTS/Entities/Departamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCORE.TESTS.Entities
+{
+    public class Departamento
+    {
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCadastro { get; set; }
+    }
+}
