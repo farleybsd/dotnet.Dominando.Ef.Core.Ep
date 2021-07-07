b67c82760748acc3fddfdc0244ec402d2730e470
farleybsd <farley.t.i@hotmail.com>
2021-07-07 00:08:05 -0300

    Gerador SQL customizado

---
+++ Sobrescrevendo_comportamentos_EF_Core/Sobrescrevendo_comportamentos_EF_Core/Comportamentos_EF_Core/MySqlServerQuerySqlGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore.Query.SqlExpressions;
+using Microsoft.EntityFrameworkCore.SqlServer.Query.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comportamentos_EF_Core
+{
+    public class MySqlServerQuerySqlGenerator : SqlServerQuerySqlGenerator
+    {
+        public MySqlServerQuerySqlGenerator(QuerySqlGeneratorDependencies dependencies) :base(dependencies)
+        {
+
+        }
+
+        protected override Expression VisitTableValuedFunction(TableValuedFunctionExpression tableValuedFunctionExpression)
+        {
+            var table = base.VisitTableValuedFunction(tableValuedFunctionExpression);
+            Sql.Append("WITH (NOLOCK)");
+            return table;
+        }
+    }
+}
