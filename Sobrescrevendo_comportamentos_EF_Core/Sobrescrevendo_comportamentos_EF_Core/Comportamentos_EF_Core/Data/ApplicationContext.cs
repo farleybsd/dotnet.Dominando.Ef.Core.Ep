901e73acb01a7d459b627fe1477eb3e2e65ce795
farleybsd <farley.t.i@hotmail.com>
2021-07-06 23:55:43 -0300

    Criando entidade e contexto

---
+++ Sobrescrevendo_comportamentos_EF_Core/Sobrescrevendo_comportamentos_EF_Core/Comportamentos_EF_Core/Data/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using Comportamentos_EF_Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comportamentos_EF_Core.Data
+{
+    public class ApplicationContext :DbContext
+    {
+        public DbSet<Departamento> Departamentos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder
+                    .LogTo(Console.WriteLine)
+                    .UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=sobrescrevendo_comportamento; Integrated Security=true")
+                    .EnableSensitiveDataLogging();
+        }
+    }
+}
